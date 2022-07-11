69bcd676d8132a5dcb8f6d85c4c1cfcfc2b0ff79
cagataygurturk <cagataygurturk@users.noreply.github.com>
2022-07-11 18:16:07 +0000

    Update Homebrew to v0.8.9

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -2,13 +2,29 @@ class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
   version "0.8.9"
-  url "https://github.com/Azure/bicep/releases/download/v0.8.9/bicep-osx-x64"
-  sha256 "970ed9ccffc4eab06662f8a32b365db0ee02bf00df8975fdce7bb4f8124c3e5c"
+
+  case
+  when OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/Azure/bicep/releases/download/v0.8.9/bicep-osx-x64"
+    sha256 "970ed9ccffc4eab06662f8a32b365db0ee02bf00df8975fdce7bb4f8124c3e5c"
+  when OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/Azure/bicep/releases/download/v0.8.9/bicep-osx-arm64"
+    sha256 "04181bdc90d789bcb010be44f6a4324d408bbdc008176a922c4f1a81270bee7a"
+  else
+    odie "Unexpected platform!"
+  end
 
   license "MIT"
 
   def install
-    bin.install "bicep-osx-x64" => "bicep"
+    case
+    when OS.mac? && Hardware::CPU.intel?
+      bin.install "bicep-osx-x64" => "bicep"
+    when OS.mac? && Hardware::CPU.arm?
+      bin.install "bicep-osx-arm64" => "bicep"
+    else
+      odie "Unexpected platform!"
+    end
   end
 
   test do
